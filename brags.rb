738ae8ddf478a8f2bbe451f824c74dd43ed7aa14
Sage Ross <sage@ragesoss.com>
2017-05-06 21:41:12 -0700

    More randomness in brags

--- brags.rb
+++ brags.rb
@@ -4,10 +4,12 @@ class Brags
   end
 
   def random
-    all_brags.sample
+    text = coin_flip ? "#{interjections.sample} #{main_subject.sample}" : "#{main_subject.sample} #{interjections.sample}"
+    text += " #{@article.url}" if lacks_title_or_url?(text)
+    text
   end
 
-  def all_brags
+  def _composed_brags
     [
 "omg wiki #{@article.title} & see wut i just did to it",
 "lol i just pwned the wiki page for #{@article.title}",
@@ -18,5 +20,43 @@ class Brags
 "if wiki wasnt so bad already theyd fix what i did to #{@article.title}",
 "what is wiki even doing? like, look at this #{@article.url}"
     ]
+  end
+
+  def main_subject
+    [
+      "what i did to #{topics.sample}",
+      "i pwned #{topics.sample} so hard",
+      "i tried to mess with #{topics.sample}",
+      "if wiki wasnt so bad already theyd fix what i did to #{topics.sample}",
+      "wiki #{@article.title} & see wut i just did to it",
+      "what is #{topics.sample} even doing? like, look at it"
+    ]
+  end
+
+  def interjections
+    %w(
+      omg lol wat haha wtf lmao WTF kek hehe wow whoa yass damn *snort*
+    )
+  end
+
+  def topics
+    [
+      "the wiki page for #{@article.title}",
+      @article.title,
+      "the #{@article.title} wiki",
+      "wiki",
+      "the wiki",
+      "the wikipedia page #{@article.title}",
+      "#{@article.title} on Wiki",
+      "this #{@article.url}"
+    ]
+  end
+
+  def coin_flip
+    [true, false][rand(2)]
+  end
+
+  def lacks_title_or_url?(text)
+    !text.include?(@article.title) && !text.include?(@article.url)
   end
 end
